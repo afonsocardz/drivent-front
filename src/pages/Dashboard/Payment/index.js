2bc5a2fb36063e1b62ed50dcdfebcbb9b2283dcf
José <romanjose3008@gmail.com>
2022-12-16 17:35:23 -0300

    feat: show message to user if dont have enroll

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,5 +1,20 @@
+import { getPersonalInformations } from '../../../services/enrollmentApi';
+import { useEffect, useState } from 'react';
+
 import PaymentForm from '../../../components/Payment';
+import NoEnrollment from '../../../components/Payment/NoEnrollment';
 
 export default function Payment() {
-  return <PaymentForm/>;
+  const [render, setRender] = useState(<></>);
+
+  useEffect(() => {
+    const userData = localStorage.getItem('userData');
+    const token = JSON.parse(userData).token;
+  
+    const promise = getPersonalInformations(token);
+    promise.then(() => setRender(<PaymentForm/>));
+    promise.catch(() => setRender(<NoEnrollment/>));
+  }, []);
+
+  return render;
 }
