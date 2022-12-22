af32e2a69737f2aa47aeb7f82669d85c7907dd27
Jean Campelo <jean93campelo@gmail.com>
2022-12-21 22:07:24 -0300

    temp: initial show rooms

--- src/components/Hotel/Rooms.js
+++ src/components/Hotel/Rooms.js
@@ -1,6 +1,24 @@
-//import { getRooms } from '../../services/roomApi';
-//import { useEffect } from 'react';
+import styled from 'styled-components';
+import { getRooms } from '../../services/roomApi.js';
+import { useState, useEffect } from 'react';
+import useToken from '../../hooks/useToken.js';
 
 export default function Rooms() {
-  return <>Rooms List</>;
+  const token = useToken();
+  //console.log(token);
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    getRooms(50, token)
+      .then((res) => setRooms(res))
+      .catch((res) => console.log(res));
+  }, []);
+
+  return <Container>{rooms.map((item) => <renderRooms />)}</Container>;
+
+  function renderRooms() {
+    return <>room</>;
+  }
 }
+
+const Container = styled.div``;
